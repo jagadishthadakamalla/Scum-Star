ff4d80d7070a8db1d5fc270b5bfcd46b66beb6b9
jagadishthadakamalla <jagadishthadakamalla@gmail.com>
2025-04-17 09:40:45 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,20 @@
+from flask import Flask, jsonify, send_file
+from backend.jira.fetch_data import get_jira_data
+from backend.reports.generate_excel import generate_sprint_report
+import os
 
+app = Flask(__name__)
+
+@app.route("/ping", methods=["GET"])
+def health_check():
+ return jsonify({"message": "Scrum Star backend is live!"})
+
+@app.route("/generate-report", methods=["GET"])
+def generate_report():
+ jira_data = get_jira_data()
+ filename = "sprint_report.xlsx"
+ report_path = generate_sprint_report(jira_data, filename)
+ return send_file(report_path, as_attachment=True)
+
+if __name__ == "__main__":
+ app.run(debug=True)
